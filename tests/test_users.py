2cc3176ec144681a76e38eba3646b78bfd5003a9
Saeid Dousti <dousti_s@yahoo.com>
2022-10-16 18:08:05 -0400

    up to 15:41 create tables, yield, drop tables

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,17 +1,54 @@
 from http import client
 from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from app.main import app # to test it
 from app import schemas
+from app.config import settings
+from app.database import get_db, Base
 
-client = TestClient(app)
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# we can hardcode this
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:password123@localhost:5432/fastapi_test"
 
-def test_root():
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base.metadata.create_all(bind=engine)
+# Base = declarative_base()
+
+# Dependency
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+# client = TestClient(app)
+
+@pytest.fixture
+def client():
+    # run our code before we return our testclient
+    Base.metadata.create_all(bind=engine) # create tables
+    yield TestClient(app)
+    Base.metadata.create_all(bind=engine) # drop tables
+
+    # run our code after our test finishes
+
+
+def test_root(client):
     res = client.get("/")
     print(res.json().get('message'))
     assert res.json().get('message') == 'Hello World which is ok!!!'
     assert res.status_code == 200
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json={"email": "hello123@gmail.com", "password":"password123"})
     new_user = schemas.UserOut(**res.json())  #15:24 do some level of validation making sure everythign looks good
     assert new_user.email == "hello123@gmail.com"
