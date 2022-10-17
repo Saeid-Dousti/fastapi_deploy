de42f32c43dc0a80c28544df50627c5e5a9ab1c0
Saeid Dousti <dousti_s@yahoo.com>
2022-10-17 18:28:45 -0400

    16:27, failed login user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -26,3 +26,19 @@ def test_login_user(client, test_user): #* 16:19
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+]) #* 16:25
+def test_incorrect_login_user(client, test_user, email, password, status_code): #* 16:22
+    #wrong password:
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
+
+
