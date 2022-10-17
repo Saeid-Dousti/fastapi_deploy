e0d21e46916e4c3d9ba2f9024d2bb883ff469c88
Saeid Dousti <dousti_s@yahoo.com>
2022-10-17 18:07:27 -0400

    16:20, conftest to put all fixtures in

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,17 +2,9 @@ import pytest
 from jose import jwt
 from app import schemas
 from app.config import settings
-from .database import client, session
+# from .database import client, session #* 16:20
 
-@pytest.fixture
-def test_user(client): #* 16:10
-    user_data = {"email":"sanjeev@gmail.com",
-                 "password": "password123"}
-    res = client.post("/users/", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
+
 
 def test_root(client):
     res = client.get("/")
