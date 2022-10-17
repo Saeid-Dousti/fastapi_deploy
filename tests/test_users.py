57bf83e73339ff46418e2cb393d9099892d72bcd
Saeid Dousti <dousti_s@yahoo.com>
2022-10-17 13:59:18 -0400

    16:18, login test, including a fixture

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,13 +1,18 @@
 import pytest
+from jose import jwt
 from app import schemas
+from app.config import settings
 from .database import client, session
 
 @pytest.fixture
-def test_user(client):
+def test_user(client): #* 16:10
     user_data = {"email":"sanjeev@gmail.com",
                  "password": "password123"}
     res = client.post("/users/", json=user_data)
     assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 def test_root(client):
     res = client.get("/")
@@ -21,7 +26,11 @@ def test_create_user(client):
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
 
-def test_login_user(client, test_user): #* 15:53
-    res = client.post("/login", data={"username": "hello123@gmail.com", "password":"password123"})
-
+def test_login_user(client, test_user): #* 16:19
+    res = client.post("/login", data={"username": test_user['email'], "password":test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, settings.algorithm)
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
