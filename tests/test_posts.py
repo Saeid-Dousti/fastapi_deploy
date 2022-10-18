36deab7e93c874197230704d99670d37277c8404
Saeid Dousti <dousti_s@yahoo.com>
2022-10-18 14:18:53 -0400

    17:21, update posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from typing import List
 from app import schemas
@@ -67,7 +68,7 @@ def test_unauthorized_user_create_post(client, test_posts):
 
     assert res.status_code == 401
 
-def test_unauthorized_user_create_post(client, test_posts):
+def test_unauthorized_user_delete_post(client, test_posts):
     res = client.delete(f"/posts/{test_posts[0].id}")
 
     assert res.status_code == 401
@@ -86,4 +87,48 @@ def test_delete_other_user_post(authorized_client, test_user, test_posts):
     
     assert res.status_code == 403
 
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
 
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data) 
+    updated_post = schemas.Post(**res.json())
+    
+    assert res.status_code == 200
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
+
+def test_update_post(authorized_client, test_user, test_user2, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data) 
+    
+    assert res.status_code == 403
+
+def test_unauthorized_user_update_post(client, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    
+    res = client.put(f"/posts/{test_posts[0].id}", json=data)
+
+    assert res.status_code == 401
+
+def test_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    
+    res = authorized_client.put("/posts/10000000000000", json=data)
+    assert res.status_code == 404
