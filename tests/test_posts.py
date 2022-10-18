ab83a7b84dbc0faec79b70cc48e21f7a1b7a6c28
Saeid Dousti <dousti_s@yahoo.com>
2022-10-18 12:35:21 -0400

    16:59, get all or one post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -13,3 +13,21 @@ def test_get_all_posts(authorized_client, test_posts):
     assert len(posts_list) == len(test_posts)
     assert res.status_code ==  200
 
+def test_unauthorized_user_get_all_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/post/99999")
+    assert res.status_code == 404
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    print(res.json())
+    post = schemas.PostOut(**res.json())
+
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
+
+    assert res.status_code == 200
