7a3dba584902f60fec0544608dd7d918e879cdb9
Saeid Dousti <dousti_s@yahoo.com>
2022-10-19 05:20:21 -0400

    18:49, CICD  and deployment to ubuntu,  build-deploy.yml

--- app/main.py
+++ app/main.py
@@ -39,4 +39,4 @@ app.include_router(vote.router) #
 
 @app.get("/")
 def root():
-    return {"message": "Hello World successfully deployed from CI/CD pipeline"}
+    return {"message": "Hello World successfully deployed to ubuntu from CI/CD pipeline"}
